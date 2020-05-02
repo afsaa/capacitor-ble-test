f760da032030e4855cb7581105b274bf2110c14d
Andres Fernando Saa <andressaa.dev@gmail.com>
2020-05-02 12:24:04 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BLE } from '@ionic-native/ble/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,8 +18,9 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BLE
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
